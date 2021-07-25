786484f0a655eae2400e15f2404057d9499e61b7
lnxcz <in@lnx.rip>
2021-07-25 19:45:32 +0200

    head

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import wave from "../public/images/wave.png"
+
+
+
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Atsukoro</title>
-        <meta name="description" content="Best Discord anime bot" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Image src={wave} alt={"wave"} layout={"fill"}/>
+      <div className={styles.container}>
+        <Head>
+          <title>Atsukoro</title>
+          <meta name="description" content="Best Discord anime bot" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
+          <div className={styles.head}>
+            <h1>Atsukoro</h1>
+            <h2>Best Anime bot</h2>
+          </div>
+      
+      </div>
 
-      <footer className={styles.footer}>
-      </footer>
-    </div>
+    </>
+
   )
 }
