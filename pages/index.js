bcacd0ec4d0f02795507230e6354174764d7364b
lnxcz <in@lnx.rip>
2021-07-25 20:39:06 +0200

    css change

--- pages/index.js
+++ pages/index.js
@@ -11,23 +11,26 @@ export default function Home() {
   return (
     <>
       <div className={styles.wave}/>
-      <div className={styles.container}>
-        <Head>
-          <title>Atsukoro</title>
-          <meta name="description" content="Best Discord anime bot" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+          <Head>
+            <title>Atsukoro</title>
+            <meta name="description" content="Best Discord anime bot" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <div className={styles.container}>
+            <div className={styles.head}>
+              <h1>Atsukoro</h1>
+              <h2>Best Anime bot</h2>
+              <div className={styles.buttons}>
 
-          <div className={styles.head}>
-            <h1>Atsukoro</h1>
-            <h2>Best Anime bot</h2>
-            <div className={styles.buttons}>
-
+              </div>
             </div>
-          </div>
-      
-      </div>
+        
+        </div>
+        <div className={styles.section}>
+          <div className={styles.container}>
 
+          </div>
+        </div>
     </>
 
   )
