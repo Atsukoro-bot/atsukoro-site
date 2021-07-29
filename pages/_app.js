89fb1a7b4fd7ad90598aa6f632475f1cf8ccb271
lnxcz <in@lnx.rip>
2021-07-29 20:22:00 +0200

    remove layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
+import 'nprogress/nprogress.css';
+import Header from '../components/Header/Header'
+import NProgress from "nprogress"
+import Router from 'next/router';
 import '../styles/globals.css'
-import Layout from '../components/Layout/Layout'
+
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function Atsukoro({ Component, pageProps }) {
   return (
     <>
-      <Layout>
+      <Header />
         <Component {...pageProps} />
-      </Layout>
     </>
   )
 }
