6caec4b70b3ba0f45d5b2426715eae4a8e53fc72
lnxcz <in@lnx.rip>
2021-07-29 14:16:10 +0200

    fetch data from api

--- pages/commands.js
+++ pages/commands.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 import styles from '../styles/Commands.module.css'
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,8 +10,33 @@ export default function Home() {
         <meta name="description" content="Best Discord anime bot" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      {data.map((cmd) => (
+        <div className={styles.data}>
+          <p>{cmd.name}</p>
+        </div>
+        ))}
 
     </div>
   )
+}
+
+
+export async function getStaticProps(context) {
+  const res = await fetch(`https://atsukoro.herokuapp.com/commands`, {
+    method: 'POST'
+  });
+
+  const data = await res.json()
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  
+  return {
+    props: { data }
+  }
 }
