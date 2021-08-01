9c57032e96f973d130f50d1b1cedc0b0b8149aec
lnx <in@lnx.rip>
2021-08-02 00:53:40 +0200

    minor changes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,44 +1,46 @@
-import styles from "../../styles/header.module.css"
-import 'react-tippy/dist/tippy.css'
-import { Tooltip } from 'react-tippy';
-import Link from 'next/link'
+import styles from "../../styles/header.module.css";
+import "react-tippy/dist/tippy.css";
+import { Tooltip } from "react-tippy";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image"
+import Image from "next/image";
 
+import logo from "../../public/images/logo.png";
 
-import logo from "../../public/images/logo.png"
+const Header: React.FC<{}> = ({}) => {
+  const router = useRouter();
 
-interface HeaderProps { }
-
-const Header: React.FC<HeaderProps> = ({ }) => {
-    const router = useRouter();
-
-    return (
-        <nav className={styles.header}>
-            <div className={styles.items}>
-                <div className={styles.logo}>
-                    <Link href="/" passHref>
-                        <Image src={logo} alt="Logos" />
-                    </Link>
-                </div>
-                <ul>
-                    <li className={router.pathname == "/" ? styles.active : ""}><Link href="/" passHref>Home</Link> </li>
-                    <li className={router.pathname == "/commands" ? styles.active : ""}><Link href="/commands" passHref>Commands</Link> </li>
-                    <li className={router.pathname == "/support" ? styles.active : ""}><Link href="https://discord.gg/2396PDsVYM" passHref>Support</Link></li>
-                </ul>
-                <Tooltip
-                    title="Coming Soon™"
-                    position="bottom"
-                    trigger="mouseenter"
-                >
-                    <div className={styles.login}>
-                        Login
-                    </div>
-                </Tooltip>
-            </div>
-        </nav>
-    )
-}
-
+  return (
+    <nav className={styles.header}>
+      <div className={styles.items}>
+        <div className={styles.logo}>
+          <Link href="/" passHref>
+            <Image src={logo} alt="Logos" />
+          </Link>
+        </div>
+        <ul>
+          <li className={router.pathname == "/" ? styles.active : ""}>
+            <Link href="/" passHref>
+              Home
+            </Link>{" "}
+          </li>
+          <li className={router.pathname == "/commands" ? styles.active : ""}>
+            <Link href="/commands" passHref>
+              Commands
+            </Link>{" "}
+          </li>
+          <li className={router.pathname == "/support" ? styles.active : ""}>
+            <Link href="https://discord.gg/2396PDsVYM" passHref>
+              Support
+            </Link>
+          </li>
+        </ul>
+        <Tooltip title="Coming Soon™" position="bottom" trigger="mouseenter">
+          <div className={styles.login}>Login</div>
+        </Tooltip>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
--- components/Meta/Meta.tsx
+++ components/Meta/Meta.tsx
@@ -1,48 +1,76 @@
 import Head from "next/head";
 
 interface MetaProps {
-    title?: string,
-    description?: string
+  title?: string;
+  description?: string;
 }
 
 const Meta: React.FC<MetaProps> = ({ title, description }) => {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <link rel="icon" href="../../public/icons/favicon.ico" type="image/x-icon" />
-            <link rel="apple-touch-icon" sizes="180x180" href="../../public/icons/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="../../public/icons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="../../public/icons/favicon-16x16.png" />
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link
+        rel="icon"
+        href="../../public/icons/favicon.ico"
+        type="image/x-icon"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="../../public/icons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="../../public/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="../../public/icons/favicon-16x16.png"
+      />
 
-            {/* Settings */}
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
+      {/* Settings */}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
 
-            {/* Twitter */}
-            <meta name="twitter:card" content="summary" key="twcard" />
-            <meta name="twitter:creator" content="Atsukoro" key="twhandle" />
-            <meta name="twitter:url" content="https://atsukoro.bruh" key="twurl" />
-            <meta name="twitter:description" content={description} key="twdescription" />
-            <meta name="twitter:image" content="https://i.imgur.com/vbIuETI.png" key="twimage" />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" key="twcard" />
+      <meta name="twitter:creator" content="Atsukoro" key="twhandle" />
+      <meta name="twitter:url" content="https://atsukoro.bruh" key="twurl" />
+      <meta
+        name="twitter:description"
+        content={description}
+        key="twdescription"
+      />
+      <meta
+        name="twitter:image"
+        content="https://i.imgur.com/vbIuETI.png"
+        key="twimage"
+      />
 
-            {/* Open Graph */}
-            <meta property="og:url" content="https://atsukoro.bruh" key="ogurl" />
-            <meta property="og:image" content="https://i.imgur.com/vbIuETI.png" key="ogimage" />
-            <meta property="og:title" content={title} key="ogtitle" />
-            <meta property="og:description" content={description} key="ogdesc" />
+      {/* Open Graph */}
+      <meta property="og:url" content="https://atsukoro.bruh" key="ogurl" />
+      <meta
+        property="og:image"
+        content="https://i.imgur.com/vbIuETI.png"
+        key="ogimage"
+      />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta property="og:description" content={description} key="ogdesc" />
 
-            {/* Other */}
-            <meta property="theme-color" content="#404EED" key="thcolor" />
-
-
-        </Head>
-    )
-}
+      {/* Other */}
+      <meta property="theme-color" content="#404EED" key="thcolor" />
+    </Head>
+  );
+};
 
 Meta.defaultProps = {
-    title: 'Atsukoro',
-    description: 'Anime, manga & japanese culture based discord bot.',
-}
+  title: "Atsukoro",
+  description: "Anime, manga & japanese culture based discord bot.",
+};
 
 export default Meta;
